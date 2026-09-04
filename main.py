8ad4bf40a8f74d5efc30ff7bf24dc2aee721e5fb
souravbin46-dot <souravbin46@gmail.com>
2026-09-04 21:03:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ BASE_HEADERS = {
 }
 
 # ─── PERFORMANCE SETTINGS ──────────────────────────────
-CONCURRENT = 600               # parallel requests (adjust for Railway)
+CONCURRENT = 500               # parallel requests (adjust for Railway)
 TOTAL_REQUESTS = 0             # 0 = infinite
 USE_PROXIES = False
 PROXY_FILE = "proxies.txt"
